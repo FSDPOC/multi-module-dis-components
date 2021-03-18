d889a72495c783abde55749d14b721a7eb9da288
Raghavendra Madugundu <raghavendra.madugundu@ingka.com>
2021-03-18 16:25:49 -0300

    InfoController.java updated

--- application-service/src/main/java/com/db/controller/InfoController.java
+++ application-service/src/main/java/com/db/controller/InfoController.java
@@ -1,7 +1,7 @@
 package com.db.controller;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +21,26 @@ public class InfoController {
     private String mavenVersion;
 
     @GetMapping("/health-info")
-    public ResponseEntity getInfo() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n[Multi Module Application project details : multi-module-dis-components]");
-        sb.append("\nApplication name : " + applicationName);
-        sb.append("\nBuild version    : " + buildVersion);
-        sb.append("\nBuild timestamp  : " + buildTimestamp);
-        sb.append("\nMaven version  : " + mavenVersion);
-        return ResponseEntity.ok(sb);
+    public Map<String, Object> getInfo() {
+        //        StringBuilder sb = new StringBuilder();
+        //        sb.append("\n[Multi Module Application project details :
+        // multi-module-dis-components]");
+        //        sb.append("\nApplication name : " + applicationName);
+        //        sb.append("\nBuild version    : " + buildVersion);
+        //        sb.append("\nBuild timestamp  : " + buildTimestamp);
+        //        sb.append("\nMaven version  : " + mavenVersion);
+        //        return ResponseEntity.ok(sb);
+
+        return Map.of(
+                "Multi Module Application project details",
+                String.join(",", "multi-module-dis-components"),
+                "Application name",
+                String.join(",", applicationName),
+                "Build version",
+                String.join(",", buildVersion),
+                "Build timestamp",
+                String.join(",", buildTimestamp),
+                "Maven version",
+                String.join(",", mavenVersion));
     }
 }

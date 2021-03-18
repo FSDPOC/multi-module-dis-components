786d2041216f5acd5072837f802497d5fd290ad4
Raghavendra Madugundu <raghavendra.madugundu@ingka.com>
2021-03-18 15:39:44 -0300

    BuildInfoController updated with map object

--- application-service/src/main/java/com/db/controller/BuildInfoController.java
+++ application-service/src/main/java/com/db/controller/BuildInfoController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.boot.info.GitProperties;
 import org.springframework.core.env.Environment;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,28 +14,37 @@ public class BuildInfoController {
     @Autowired BuildProperties buildProperties;
 
     @GetMapping("/build-info")
-    public ResponseEntity getBuildInfo() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n[Multi Module Application project details : multi-module-dis-components]");
-        sb.append("\nApplication name : " + buildProperties.getName());
-        sb.append("\nBuild version    : " + buildProperties.getVersion());
-        sb.append("\nBuild timestamp  : " + buildProperties.getTime());
-        sb.append("\nMaven version  : " + buildProperties.get("maven.version"));
-        sb.append("\nRaghu Machine OS  : " + buildProperties.get("raghu.machine"));
-        return ResponseEntity.ok(sb);
+    public Map<String, Object> getBuildInfo() {
+        return Map.of(
+                "Multi Module Application project details",
+                String.join(",", "multi-module-dis-components"),
+                "Application name",
+                String.join(",", buildProperties.getName()),
+                "Build version",
+                String.join(",", buildProperties.getVersion()),
+                "Build timestamp",
+                String.join(",", buildProperties.getTime().toString()),
+                "Maven version",
+                String.join(",", buildProperties.get("maven.version")),
+                "Raghu Machine OS",
+                String.join(",", buildProperties.get("raghu.machine")));
     }
 
     @Autowired private GitProperties gitProperties;
 
     @GetMapping("/git-info")
-    public ResponseEntity gitInfo() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("\n[Multi Module Application project details : multi-module-dis-components]");
-        sb.append("\nBranch name : " + this.gitProperties.getBranch());
-        sb.append("\nBuild host    : " + this.gitProperties.get("build.host"));
-        sb.append("\ngit Commit id  : " + this.gitProperties.getCommitId());
-        sb.append("\ngit Commit Time : " + this.gitProperties.getCommitTime());
-        return ResponseEntity.ok(sb);
+    public Map<String, Object> gitInfo() {
+        return Map.of(
+                "Multi Module Application project details",
+                String.join(",", "multi-module-dis-components"),
+                "Branch name",
+                String.join(",", gitProperties.getBranch()),
+                "Build host",
+                String.join(",", gitProperties.get("build.host")),
+                "git Commit id",
+                String.join(",", gitProperties.getCommitId()),
+                "git Commit Time",
+                String.join(",", gitProperties.getCommitTime().toString()));
     }
 
     @Autowired private Environment environment;
